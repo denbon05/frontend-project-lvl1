183f83854eb0991f4e1bbd6c3458157a69f33b80
den <denisamigoua@gmail.com>
2020-04-26 20:01:28 +0200

    create src directory, and file which asked name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
-#!/usr/bin/ node
+#!/usr/bin/env node
+import helloName from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!")
+console.log("Welcome to the Brain Games!");
+helloName();
+
